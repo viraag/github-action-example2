6050a8d77fc8825cbb578d4ec9ed48d0d878d5db
Prashant Kumar <viraagsingh007@gmail.com>
2022-01-09 20:06:34 +0530

    code

---
+++ src/main/java/com/vg/CICDDemo/CICDdemo/CicDdemoApplication.java
@@ -0,0 +1,30 @@
+package com.vg.CICDDemo.CICDdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CicDdemoApplication {
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome VG";
+	}
+
+	/**
+	 * echo "# github-actions-example" >> README.md
+	 * git init
+	 * git add README.md
+	 * git commit -m "first commit"
+	 * git branch -M main
+	 * git remote add origin https://github.com/viraag/github-actions-example.git
+	 * git push -u origin main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(CicDdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vg/CICDDemo/CICDdemo/CicDdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vg.CICDDemo.CICDdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
